eb581225ce4ed285d111265dcae5d1432b13aca3
Jean Franco <jean.s.franco@gmail.com>
2025-06-19 18:59:13 -0300

    refactor(routing): rename Route to ReactRouterRoute for clarity

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,8 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route as ReactRouterRoute } from "react-router-dom";
+
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
 import { FinalScriptPage } from "@/pages/final-script-page";
@@ -13,9 +14,9 @@ import { FinalScriptPage } from "@/pages/final-script-page";
 const AppRoutes: FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/final-script" element={<FinalScriptPage />} />
-      <Route path="*" element={<NotFound />} />
+      <ReactRouterRoute path="/" element={<Home />} />
+      <ReactRouterRoute path="/final-script" element={<FinalScriptPage />} />
+      <ReactRouterRoute path="*" element={<NotFound />} />
     </Routes>
   );
 }
